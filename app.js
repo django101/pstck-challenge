e5e3f7926fb22436e4cbba7b67adf0b57181fa02
django101 <strategsolutions@outlook.com>
2019-02-27 19:16:16 +0100

    Database auto-disconnect check

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function handleDisconnect() {
 // connect to database
     db.connect((err) => {
         if (err) {
-        // throw err;
+            // throw err;
             console.log('error when connecting to db:', err);
             setTimeout(handleDisconnect, 2000);
         }
@@ -57,17 +57,18 @@ function handleDisconnect() {
 
 
     db.on('error', function (err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
 }
 
 handleDisconnect();
+
+
 
 global.db = db;
 

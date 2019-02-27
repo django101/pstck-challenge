8a636ca412f295248deddf323e0d0bb7a3ae89ad
django101 <strategsolutions@outlook.com>
2019-02-27 09:23:08 +0100

    Fix Static Port Number

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 const { getHomePage } = require('./routes/index');

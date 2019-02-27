19be3d7f36f6d98e32c2a074bee301041f49e62d
django101 <strategsolutions@outlook.com>
2019-02-27 09:32:55 +0100

    Trying to fix errors

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 ﻿module.exports = {
     getHomePage: (req, res) => {
+        if(req.cookies["bakery_user_sname"])
         res.render('index.ejs', { displayName: req.cookies['bakery_user_sname'] });
+        else
+        res.redirect("/login");
     }
 };

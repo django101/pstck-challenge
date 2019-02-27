a4f187af28127e390b585fed64dc6e4c8f928d31
django101 <strategsolutions@outlook.com>
2019-02-27 19:38:36 +0100

    Fix Database Disconnect

--- app.js
+++ app.js
@@ -44,30 +44,38 @@ const db = mysql.createConnection({
 });
 
 
-function handleDisconnect() {
-// connect to database
-    db.connect((err) => {
-        if (err) {
-            // throw err;
-            console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000);
-        }
-        console.log('Connected to database');
-    });
+// function handleDisconnect() {
+// // connect to database
+//     db.connect((err) => {
+//         if (err) {
+//             // throw err;
+//             console.log('error when connecting to db:', err);
+//             setTimeout(handleDisconnect, 2000);
+//         }
+//         console.log('Connected to database');
+//     });
 
 
-    db.on('error', function (err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
-        }
-    });
-}
+//     db.on('error', function (err) {
+//         console.log('db error', err);
+//         if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+//             handleDisconnect();                         // lost due to either server restart, or a
+//         } else {                                      // connnection idle timeout (the wait_timeout
+//             throw err;                                  // server variable configures this)
+//         }
+//     });
+// }
 
-handleDisconnect();
+// handleDisconnect();
 
+
+db.connect((err) => {
+    if (err) {
+        // throw err;
+        console.log('error when connecting to db:', err);
+    }
+    console.log('Connected to database');
+});
 
 
 global.db = db;
